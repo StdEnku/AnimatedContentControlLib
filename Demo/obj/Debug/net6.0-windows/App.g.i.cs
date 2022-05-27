0d7aff29ae22bfc03f3fd5fa038b76e9bdc36ccd
StdEnku <SyplessEleki@gmail.com>
2022-05-28 03:11:56 +0900

    add gitignore

--- Demo/obj/Debug/net6.0-windows/App.g.i.cs
+++ Demo/obj/Debug/net6.0-windows/App.g.i.cs
@@ -63,7 +63,7 @@ namespace Demo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Demo;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Demo;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
