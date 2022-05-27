b3652688dc0d2722d8d089ba9b98f054ac2c3bef
StdEnku <SyplessEleki@gmail.com>
2022-05-27 19:30:53 +0900

    ドキュメントコメント追記

--- AnimatedContentControlLib.Wpf/Controls/StoryboardList.cs
+++ AnimatedContentControlLib.Wpf/Controls/StoryboardList.cs
@@ -3,6 +3,9 @@ using System.Windows.Media.Animation;
 
 namespace AnimatedContentControlLib.Wpf.Controls;
 
+/// <summary>
+/// XamlでList<Storyboard>を使用するためのエイリアス
+/// </summary>
 public class StoryBoardList : List<Storyboard>
 {
 }
