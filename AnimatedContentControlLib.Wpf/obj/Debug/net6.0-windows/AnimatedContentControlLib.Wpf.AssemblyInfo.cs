ed83dfbc6131578c8468d78ebd6dc410b7d2c65b
StdEnku <SyplessEleki@gmail.com>
2022-07-18 18:50:05 +0900

    パッケージ情報更新

--- AnimatedContentControlLib.Wpf/obj/Debug/net6.0-windows/AnimatedContentControlLib.Wpf.AssemblyInfo.cs
+++ AnimatedContentControlLib.Wpf/obj/Debug/net6.0-windows/AnimatedContentControlLib.Wpf.AssemblyInfo.cs
@@ -16,11 +16,12 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2022 StdEnku")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Library that provides AnimatedContentControl that can perform animation when the " +
     "Content property is changed.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimatedContentControlLib.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimatedContentControlLib.Wpf")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/StdEnku/AnimatedContentControlLib")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
