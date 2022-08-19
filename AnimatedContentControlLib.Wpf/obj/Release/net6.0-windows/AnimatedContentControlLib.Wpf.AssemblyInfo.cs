4720e2d208cc943e2fddd213596cf7ad2d0e9c99
StdEnku <SyplessEleki@gmail.com>
2022-08-19 17:32:10 +0900

    アセンブリ情報修正

--- AnimatedContentControlLib.Wpf/obj/Release/net6.0-windows/AnimatedContentControlLib.Wpf.AssemblyInfo.cs
+++ AnimatedContentControlLib.Wpf/obj/Release/net6.0-windows/AnimatedContentControlLib.Wpf.AssemblyInfo.cs
@@ -14,10 +14,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimatedContentControlLib.Wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2022 StdEnku")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Library that provides AnimatedContentControl that can perform animation when the " +
-    "Content property is changed.")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("ContentProperty変化時にアニメーションを実行できるAnimatedContentControl本体を提供するライブラリ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.0.3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimatedContentControlLib.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimatedContentControlLib.Wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.0.0")]
